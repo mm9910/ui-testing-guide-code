c41e01548985d2aa7e74f52aae6df0758d67d445
Mario Maffettone <mario.maffettone@kineton.it>
2023-05-29 15:46:31 +0200

    Stable version

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ const toPath = _path => path.join(process.cwd(), _path);
 module.exports = {
   staticDirs: ['../public'],
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app', '@storybook/addon-interactions'],
   webpackFinal: async config => {
     return {
       ...config,
@@ -16,6 +16,9 @@ module.exports = {
         }
       }
     };
+  },
+  features: {
+    interactionsDebugger: true,
   },
   framework: {
     name: '@storybook/react-webpack5',
